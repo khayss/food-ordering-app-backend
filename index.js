db2cde036244154691c485ec2e627b8eebc539ce
KayPrime <forendals@gmail.com>
2024-06-21 15:49:58 +0100

    update: update front end origin from development to live

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const app = express();
 app
   .use(
     cors({
-      // origin: "https://food-app-front-end-sand.vercel.app",
-      origin: "http://localhost:3000",
+      origin: "https://food-app-front-end-sand.vercel.app",
+      // origin: "http://localhost:3000",
       credentials: true,
     })
   )
